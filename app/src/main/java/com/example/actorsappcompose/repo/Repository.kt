3f5e92f116287f07c2c0f3462dc4e6117d410c4d
ozancanguz <ozancanguz24@gmail.com>
2023-09-23 17:23:53 +0300

    viewomodel created

--- app/src/main/java/com/example/actorsappcompose/repo/Repository.kt
+++ app/src/main/java/com/example/actorsappcompose/repo/Repository.kt
@@ -1,8 +1,11 @@
 package com.example.actorsappcompose.repo
 
 import com.example.actorsappcompose.remote.RemoteDataSource
+import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
+
+@ViewModelScoped
 class Repository@Inject constructor(remoteDataSource: RemoteDataSource) {
 
 
---
+++ app/src/main/java/com/example/actorsappcompose/viewmodels/CharacterViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.actorsappcompose.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.actorsappcompose.data.Character
+import com.example.actorsappcompose.data.CharacterItem
+import com.example.actorsappcompose.repo.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CharacterViewModel @Inject constructor(val repository: Repository): ViewModel(){
+
+    private val _state= MutableStateFlow(Character())
+    val state:StateFlow<Character>
+         get()=_state
+
+
+    fun getCharacters(){
+        viewModelScope.launch {
+            val response=repository.remote.getCharacters()
+            if(response.isSuccessful){
+                _state.value= response.body()!!
+            }else{
+                Log.d("viewmodel","No data")
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
